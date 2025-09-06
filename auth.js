d999ea57e3eea1fe8c3e8c070a6d78ebfea5e393
ym1643040001000-ops <ym1643040001000@gmail.com>
2025-09-07 00:57:20 +0300

    auth.js

---
+++ auth.js
@@ -0,0 +1,16 @@
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+import { app } from "./firebase.js";
+
+const auth = getAuth(app);
+
+export async function register(email, password) {
+  return await createUserWithEmailAndPassword(auth, email, password);
+}
+
+export async function login(email, password) {
+  return await signInWithEmailAndPassword(auth, email, password);
+}
+
+export function watchAuth(callback) {
+  onAuthStateChanged(auth, callback);
+}
