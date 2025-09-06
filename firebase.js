1dd5a8679a7687c304a7d511ef18892ff19b65e3
ym1643040001000-ops <ym1643040001000@gmail.com>
2025-09-06 23:46:38 +0300

    firebase.js

--- firebase.js
+++ firebase.js
@@ -1,13 +1,14 @@
-import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-import { getDatabase } from "firebase/database";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-auth.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-database.js";
+import { getStorage } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-storage.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyAOGHBrIW5aLPyhqC6fbmbglRdOhWv0XXU",
   authDomain: "eazy-cash-8c5fa.firebaseapp.com",
   databaseURL: "https://eazy-cash-8c5fa-default-rtdb.firebaseio.com",
   projectId: "eazy-cash-8c5fa",
-  storageBucket: "eazy-cash-8c5fa.firebasestorage.app",
+  storageBucket: "eazy-cash-8c5fa.appspot.com",
   messagingSenderId: "693700754830",
   appId: "1:693700754830:web:2eb64b511ecc7d325338df",
   measurementId: "G-ZEHXF93PFJ"
@@ -17,3 +18,4 @@ const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
 export const db = getDatabase(app);
+export const storage = getStorage(app);
